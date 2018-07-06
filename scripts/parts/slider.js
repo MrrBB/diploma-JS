963737c0f9daaad3ee18d2939689dc59f1397384
Тимур <Dotsaev_timur@bk.ru>
2018-07-06 18:40:23 +0400

    диплом

--- scripts/parts/slider.js
+++ scripts/parts/slider.js
@@ -1,52 +1,55 @@
 function slider() {
 
-	let slideIndex = 1,
-	slides = document.getElementsByClassName('main-slider-item'),
-	nextBtn = document.getElementById('sliderNext'),
-	prevBtn = document.getElementById('sliderPrev'),
 
-	bottomSlider = document.getElementsByClassName("feedback-slider-item"),
-	btnNext = document.querySelector('.main-next-btn'),
-	btnPrev = document.querySelector(".main-prev-btn");
-	showSlides(slideIndex);
-	bottomSliderList(slideIndex);
-function showSlides(n) {
-		if(n > slides.length) {slideIndex = 1;}
-		if(n < 0) {slideIndex = slides.length;}
-		for(let i = 0; i < slides.length; i++) {slides[i].style.display = "none";}
-		slides[slideIndex  - 1].style.display = 'flex';		
-		let start = Date.now();	
-}
-	  
-function bottomSliderList(n){
-		if(n > bottomSlider.length) {slideIndex = 1;}
-		if(n < 0) {slideIndex = bottomSlider.length;}
-		for(let i = 0; i < bottomSlider.length; i++) {bottomSlider[i].style.display = "none";}
-		bottomSlider[slideIndex  - 1].style.display = 'flex';
-		let start = Date.now();	
-}
+	let slides = document.getElementsByClassName('feedback-slider-item');
+	let slideNum = 1;
+	showSlides(slideNum);
+	prevSlides(slideNum);
 
-	setInterval(plus,'5000');
+
+	function showSlides(n) {
+		if(n > slides.length) {slideNum = 1;}
+    	if(n < 0) {slideNum = slides.length;}
+		for(let i = 0; i < slides.length; i++){
+			slides[i].style.display = "none";
+			slides[i].classList.remove("animated", "bounceInRight");
+		}
+		slides[slideNum  - 1].style.display = 'flex';
+		slides[slideNum - 1].classList.add("animated", "bounceInRight");
+	}
 	function plusSlides (n){
-		showSlides(slideIndex += n)
-	}
+		showSlides(slideNum += n)
+  	}
 
-	function plus(){
-	return plusSlides(+1)
+	function next(){
+		return plusSlides(+1)
 	};
 
-	function plusSlidesBottom (n){
-		bottomSliderList(slideIndex += n)
+	function prevSlides(n) {
+		if(n > slides.length) {slideNum = 1;}
+    	if(n < 1) {slideNum = slides.length}
+		for(let i = 0; i < slides.length; i++){
+			slides[i].style.display = "none";
+			slides[i].classList.remove("animated", "bounceInRight");
+		}
+		slides[slideNum  - 1].style.display = 'flex';
+		slides[slideNum - 1].classList.add("animated", "bounceInLeft");
 	}
-	function plusBottom(){
-		return plusSlidesBottom(+1);
+	function minusSlides(n){
+		prevSlides(slideNum += n)
+  	}
 
-	};
-	btnNext.addEventListener('click', plusBottom);
-	btnPrev.addEventListener('click', function() {
-		plusSlidesBottom(-1)
-	})
-	setInterval(plusBottom,'5000');
+	function prev(){
+		minusSlides(-1)
+	}
+	document.getElementById('nextBTN').addEventListener('click', next);
+	document.getElementsByClassName("main-prev-btn")[0].addEventListener('click', prev);
+	setInterval(next,'7000');
+
+
+
+
+
 
 } 
-module.exports = slider;
+slider();
